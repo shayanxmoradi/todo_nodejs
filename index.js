4dcb4c68866da6f9c2100b1e3e60bf00bb7b768f
shayanxmoradi <shayan.moradi92@gmail.com>
2024-08-23 09:24:52 +0330

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let name = "shayan"
+for (let i = 0; i < 10; i++) {
+
+    console.log('Thank you '+name+' for using WebStorm 💙')
+}
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+}
+console.log(randomInt(5))
+
+
